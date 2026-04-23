c958b9099c4487fc4791b526cc1ebbdbc029fdda
ranu1490 <ranusinghrajput75@gmail.com>
2026-04-23 20:41:01 +0530

    After adding spring security

---
+++ spring-security-6/src/main/java/com/authservice/springsecurity6/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.authservice.springsecurity6.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("")
+    public String welcome(){
+        return "Welcome to Spring Security 6 with JWT Authentication!";
+    }
+}
